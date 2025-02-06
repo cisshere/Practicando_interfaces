ca3224e4ebacd4f87fd0151c9d58971f0e6d4dcd
Shere Cis <cis.shere@gmail.com>
2025-02-06 16:05:06 -0300

    corregido el error de escritura

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,71 +1,71 @@
 export interface Persona {
-    id: number;
-    firstName: string;
-    lastName: string;
-    maidenName: string;
-    age: number;
-    gender: string;
-    email: string;
-    phone: number;
-    username: string;
-    password: string;
-    birthDate: number;
-    image: string;
-    bloodgroup: string;
-    heigth: number;
-    weigth: number;
-    eyeColor: string;
-    hair: Hair;
-    ip: string;
-    address: Address;
-    macAddress: string;
-    university: string;
-    bank: Bank;
-    company: Company;
-    ein: string;
-    ssn: string;
-    userAgent: string;
-    crypto: Crypto;
-    role: string;
-  }
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: number;
+  username: string;
+  password: string;
+  birthDate: number;
+  image: string;
+  bloodGroup: string;
+  heigth: number;
+  weigth: number;
+  eyeColor: string;
+  hair: Hair;
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: Bank;
+  company: Company;
+  ein: string;
+  ssn: string;
+  userAgent: string;
+  crypto: Crypto;
+  role: string;
+}
 
-  export interface Hair {
-    color: string;
-    type: string;
-  }
+export interface Hair {
+  color: string;
+  type: string;
+}
 
-  export interface Address{
-    address : string;
-    city: number;
-    state: string;
-    stateCode: string;
-    postalCode: number;
-    coordinates: Coordinates;
-    country: string;
-   }
+export interface Address {
+  address: string;
+  city: number;
+  state: string;
+  stateCode: string;
+  postalCode: number;
+  coordinates: Coordinates;
+  country: string;
+}
 
-   export interface Coordinates {
-    lat: number;
-    ing: number;
-   }
+export interface Coordinates {
+  lat: number;
+  ing: number;
+}
 
-   export interface Bank{
-    cardExpire: string;
-    cardNumber: number;
-    cardType: string;
-    currency: string;
-    iban: string;
-   }
+export interface Bank {
+  cardExpire: string;
+  cardNumber: number;
+  cardType: string;
+  currency: string;
+  iban: string;
+}
 
-   export interface Company{
-    departament: string;
-    name: string;
-    title: string;
-    adress: Address;
-   }
+export interface Company {
+  departament: string;
+  name: string;
+  title: string;
+  adress: Address;
+}
 
-   export interface Crypto{
-    coin: string;
-    wallet: string;
-    network: string;
-   }
+export interface Crypto {
+  coin: string;
+  wallet: string;
+  network: string;
+}
